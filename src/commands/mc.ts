4fce367819d339572a4cd4cca9f56693b845648c
Bart Dorsey <bart@bartdorsey.com>
2022-03-21 19:50:15 +0000

    UPdating mc command

--- src/commands/mc.ts
+++ src/commands/mc.ts
@@ -6,10 +6,16 @@ export default {
   description: 'minecraft server commands',
   cooldown: 1,
   async execute(message: { channel: { send: (arg0: string) => void; }; }, args: any) {
+    const command = args.join(" ");
+    console.log(`Sending ${command} to minecraft.`);
     const { data } = await axios.post(mcAPI, {
-      command: args[0]
+      command
     });
     console.log(data);
+    if (data.response === '\r\n') {
+      message.channel.send("Error: empty response");
+      return;
+    }
     message.channel.send(data.response);
   },
 }
